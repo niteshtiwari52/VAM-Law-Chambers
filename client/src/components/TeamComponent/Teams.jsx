58e033aa8680bc9f74f34bf654aa735125435029
niteshtiwari52 <niteshtiwari5222@gmail.com>
2024-05-18 19:26:26 +0530

    About us section

--- client/src/components/TeamComponent/Teams.jsx
+++ client/src/components/TeamComponent/Teams.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import { LiaLinkSolid } from "react-icons/lia";
 
 import { FaLinkedinIn } from "react-icons/fa";
+
+import { NavLink } from "react-router-dom";
 const Teams = () => {
+  const Teams = [
+    {
+      id: 1,
+      name: "Nitesh Tiwari1",
+      designation: "Managing Director",
+      linkedInURL: "https:/linkdin.com/niteshtiwari5222",
+      profileURL: `/user`,
+    },
+    {
+      id: 2,
+      name: "Nitesh Tiwari2",
+      designation: "Managing Director",
+      linkedInURL: "https:/linkdin.com/niteshtiwari5222",
+      profileURL: `/user`,
+    },
+  ];
   return (
     <>
       <div className="">
@@ -22,43 +40,33 @@ const Teams = () => {
             </p>
           </div>
           {/* images */}
-          <div className="w-2/3 h-[460px] mx-auto flex flex-wrap gap-4">
+
+          <div className="w-2/3 h-[460px] mx-auto flex  gap-4">
             {/* member 1 */}
-            <div className="w-1/2 rounded-md bg-gray-300 relative">
-              {/* <img src="" alt="" /> */}
-              <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-                <div className="text-center my-3">
-                  <p className="font-normal text-sm">Managaing Director</p>
-                  <h3 className="font-medium text-xl">Nitesh Tiwari</h3>
-                </div>
-                <div className=" flex justify-center items-center gap-4">
-                  <div className="bg-blue-700 w-10 h-10 p-2 rounded-full flex justify-center items-center">
-                    <FaLinkedinIn className="text-white w-6 h-6" />
+            {Teams.map((item) => (
+              <div className="w-1/2 rounded-md bg-gray-300 relative">
+                {/* <img src="" alt="" /> */}
+
+                <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+                  <div className="text-center my-3">
+                    <p className="font-normal text-sm">{item.designation}</p>
+                    <h3 className="font-medium text-xl">{item.name}</h3>
                   </div>
-                  <div className="bg-gray-500 w-10 h-10 p-2 rounded-full flex justify-center items-center">
-                    <LiaLinkSolid className="text-white w-6 h-6" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* member 2 */}
-            <div className="w-1/2 rounded-md bg-gray-300 relative">
-              {/* <img src="" alt="" /> */}
-              <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-                <div className="text-center my-3">
-                  <p className="font-normal text-sm">Managaing Director</p>
-                  <h3 className="font-medium text-xl">Nitesh Tiwari</h3>
-                </div>
-                <div className=" flex justify-center items-center gap-4">
-                  <div className="bg-blue-700 w-10 h-10 p-2 rounded-full flex justify-center items-center">
-                    <FaLinkedinIn className="text-white w-6 h-6" />
-                  </div>
-                  <div className="bg-gray-500 w-10 h-10 p-2 rounded-full flex justify-center items-center">
-                    <LiaLinkSolid className="text-white w-6 h-6" />
+                  <div className=" flex justify-center items-center gap-4">
+                    <NavLink to={item.linkedInURL}>
+                      <div className="bg-blue-700 w-10 h-10 p-2 rounded-full flex justify-center items-center">
+                        <FaLinkedinIn className="text-white w-6 h-6" />
+                      </div>
+                    </NavLink>
+                    <NavLink to={item.profileURL}>
+                      <div className="bg-gray-500 w-10 h-10 p-2 rounded-full flex justify-center items-center">
+                        <LiaLinkSolid className="text-white w-6 h-6" />
+                      </div>
+                    </NavLink>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
