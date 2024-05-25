6dd73d6fbe202bc25713ad31a073fa1020542114
niteshtiwari52 <niteshtiwari5222@gmail.com>
2024-05-25 19:24:20 +0530

    contact page update

--- client/src/components/ContactComponent/Contact.jsx
+++ client/src/components/ContactComponent/Contact.jsx
@@ -109,20 +109,20 @@ const Contact = () => {
           <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
             <div className="lg:col-span-2 lg:py-12">
               <p className="max-w-xl text-lg">
-                At the same time, the fact that we are wholly owned and totally
-                independent from manufacturer and other group control gives you
-                confidence that we will only recommend what is right for you.
+                Your Justice, Our Commitment - Reach Out to VAM Law Chambers
+                Today.
               </p>
 
               <div className="mt-8">
-                <a href="#" className="text-2xl font-bold text-pink-600">
+                <a
+                  href="tel:8586086124"
+                  className="text-2xl font-bold text-[#ab954b]"
+                >
                   {" "}
-                  0151 475 4450{" "}
+                  8586086124
                 </a>
 
-                <address className="mt-2 not-italic">
-                  282 Kevin Brook, Imogeneborough, CA 58517
-                </address>
+                <address className="mt-2 not-italic">Address</address>
               </div>
             </div>
 
